db1e8194296fcf117b6dea1b55f11aa6d3ecb569
James Eastham <jamesuk@amazon.com>
2022-12-09 15:29:08 +0000

    Add .NET 7 minimal API example

---
+++ src/NET7NativeMinimalAPI/Shared/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.Lambda.Serialization.SystemTextJson;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Shared.DataAccess;
+
+namespace Shared
+{
+    public static class Startup
+    {
+        public static WebApplication Build(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSingleton<ProductsDAO, DynamoDbProducts>();
+
+            builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi, options =>
+            {
+                options.Serializer = new SourceGeneratorLambdaJsonSerializer<ApiSerializerContext>();
+            });
+            
+            builder.Logging.ClearProviders();
+            builder.Logging.AddJsonConsole(options =>
+            {
+                options.IncludeScopes = true;
+                options.UseUtcTimestamp = true;
+                options.TimestampFormat = "hh:mm:ss ";
+            });
+
+            return builder.Build();
+        }
+    }
+}
