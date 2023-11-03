1bf57887a631a6a236ffb48a4c9692c0fd66a014
Riccardo Venturi <ricven@amazon.com>
2023-11-03 23:17:17 +0100

    .

--- loadtest/run-all-load-tests.sh
+++ loadtest/run-all-load-tests.sh
@@ -5,7 +5,7 @@ then
   echo SKIPPING net6 minimal api :$LT_NET6_MINIMAL_API
 else
   echo "RUNNING load test for net6 minimal api"
-  cd src/NET6MinimalAPI/
+  cd ../src/NET6MinimalAPI/
   source ./deploy.sh
   source ./run-load-test.sh
 fi
@@ -16,7 +16,7 @@ then
   echo SKIPPING net6 minimal api web adapter :$LT_NET6_MINIMAL_API_WEB_ADAPTER
 else
   echo "RUNNING load test for net6 minimal api web adapter"
-  cd src/NET6MinimalAPIWebAdapter/
+  cd ../src/NET6MinimalAPIWebAdapter/
   source ./deploy.sh
   source ./run-load-test.sh
 fi
