6e0077e2c828317b5f30945efe7e4b2a2412531e
Riccardo Venturi <ricven@amazon.com>
2023-11-05 09:44:55 +0100

    removing NET6 Minimal api projects to solve conflicts with Net6 NET6 folder name

--- src/NET6MinimalAPIWebAdapter/deploy.sh
+++
@@ -1,2 +0,0 @@
-sam build
-sam deploy --stack-name dotnet6-minimal-api-web-adapter --resolve-s3 --s3-prefix dotnet6-minimal-api-web-adapter --no-confirm-changeset --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM
