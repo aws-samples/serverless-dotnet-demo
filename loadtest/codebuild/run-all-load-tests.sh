c15d5952b1418cb1f72ff892f7e655b5bcf58a10
Riccardo Venturi <ricven@amazon.com>
2023-11-05 11:02:01 +0100

    improving loadtest script

--- loadtest/codebuild/run-all-load-tests.sh
+++ loadtest/codebuild/run-all-load-tests.sh
@@ -2,22 +2,28 @@ TEST_DURATIOMN_SEC=60
 LOG_INTERVAL_MIN=20
 LOG_DELETE=1
 
-if [ x"${LT_TEST_DURATIOMN_SEC}" != "x" ];  
+if [ "x${LT_TEST_DURATIOMN_SEC}" != x ];  
 then
   TEST_DURATIOMN_SEC=$LT_TEST_DURATIOMN_SEC
 fi
 
-if [ x"${LT_LOG_INTERVAL_MIN}" != "x" ];  
+if [ "x${LT_LOG_INTERVAL_MIN}" != x ];  
 then
   LOG_INTERVAL_MIN=$LT_LOG_INTERVAL_MIN
 fi
 
-if [ x"${LT_LOG_DELETE}" != "x" ];  
+if [ "x${LT_LOG_DELETE}" != xsx ];  
 then
   LOG_DELETE=$LT_LOG_DELETE
 fi
 
-if [ x"${LT_NET6_MINIMAL_API}" == "x" ];  
+echo --------------------------------------------
+echo TEST_DURATIOMN_SEC: $TEST_DURATIOMN_SEC
+echo LOG_INTERVAL_MIN: $LOG_INTERVAL_MIN
+echo LOG_DELETE: $LOG_DELETE
+echo --------------------------------------------
+
+if [ "$LT_NET6_MINIMAL_API" != yes ];  
 then
   echo SKIPPING net6 minimal api :$LT_NET6_MINIMAL_API
 else
@@ -27,8 +33,7 @@ else
   source ./run-loadtest.sh $TEST_DURATIOMN_SEC $LOG_INTERVAL_MIN $LOG_DELETE
 fi
 
-#export LT_NET6_MINIMAL_API_WEB_ADAPTER=1
-if [ x"${LT_NET6_MINIMAL_API_WEB_ADAPTER}" == "x" ];  
+if [ "$LT_NET6_MINIMAL_API_WEB_ADAPTER" != yes ];  
 then
   echo SKIPPING net6 minimal api web adapter :$LT_NET6_MINIMAL_API_WEB_ADAPTER
 else
