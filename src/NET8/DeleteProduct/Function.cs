f9e82b81408a9c91ebbc744c83d8da6c0ef66097
Beau Gosse <bgosse@amazon.com>
2024-03-14 16:26:54 -0500

    use the same Function code from .NET 6 for .NET 8

--- src/NET8/DeleteProduct/Function.cs
+++ src/NET8/DeleteProduct/Function.cs
@@ -3,85 +3,69 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using Amazon.XRay.Recorder.Handlers.AwsSdk;
-using Shared;
 using Shared.DataAccess;
 
-public class Function
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace DeleteProduct
 {
-    private static ProductsDAO dataAccess;
-
-    static Function()
+    public class Function
     {
-        AWSSDKHandler.RegisterXRayForAllServices();
-        dataAccess = new DynamoDbProducts();
-    }
-
-    /// <summary>
-    /// The main entry point for the custom runtime.
-    /// </summary>
-    /// <param name="args"></param>
-    private static async Task Main()
-    {
-        Func<APIGatewayHttpApiV2ProxyRequest, ILambdaContext, Task<APIGatewayHttpApiV2ProxyResponse>> handler = FunctionHandler;
-        await LambdaBootstrapBuilder.Create(handler, new SourceGeneratorLambdaJsonSerializer<CustomJsonSerializerContext>(options => {
-                options.PropertyNameCaseInsensitive = true;
-            }))
-            .Build()
-            .RunAsync();
-    }
-    
-    public static async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest apigProxyEvent, ILambdaContext context)
-    {
-        if (!apigProxyEvent.RequestContext.Http.Method.Equals(HttpMethod.Delete.Method))
+        private readonly ProductsDAO dataAccess;
+        public Function()
         {
-            return new APIGatewayHttpApiV2ProxyResponse
-            {
-                Body = "Only DELETE allowed",
-                StatusCode = (int)HttpStatusCode.MethodNotAllowed,
-            };
+            this.dataAccess = new DynamoDbProducts();
         }
 
-        try
+        public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest apigProxyEvent,
+            ILambdaContext context)
         {
-            context.Logger.LogLine(JsonSerializer.Serialize(apigProxyEvent, CustomJsonSerializerContext.Default.APIGatewayHttpApiV2ProxyRequest));
-            
-            var id = apigProxyEvent.PathParameters["id"];
-
-            var product = await dataAccess.GetProduct(id);
-
-            if (product == null)
+            if (!apigProxyEvent.RequestContext.Http.Method.Equals(HttpMethod.Delete.Method))
             {
                 return new APIGatewayHttpApiV2ProxyResponse
                 {
-                    Body = "Not Found",
-                    StatusCode = (int)HttpStatusCode.NotFound,
+                    Body = "Only DELETE allowed",
+                    StatusCode = (int)HttpStatusCode.MethodNotAllowed,
                 };
             }
 
-            await dataAccess.DeleteProduct(product.Id);
-
-            return new APIGatewayHttpApiV2ProxyResponse
+            try
             {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = $"Product with id {id} deleted"
-            };
-        }
-        catch (Exception e)
-        {
-            context.Logger.LogError($"Error deleting product {e.Message} {e.StackTrace}");
+                var id = apigProxyEvent.PathParameters["id"];
 
-            return new APIGatewayHttpApiV2ProxyResponse
+                var product = await dataAccess.GetProduct(id);
+
+                if (product == null)
+                {
+                    return new APIGatewayHttpApiV2ProxyResponse
+                    {
+                        Body = "Not Found",
+                        StatusCode = (int)HttpStatusCode.NotFound,
+                    };
+                }
+
+                await dataAccess.DeleteProduct(product.Id);
+        
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = $"Product with id {id} deleted"
+                };
+            }
+            catch (Exception e)
             {
-                Body = "Not Found",
-                StatusCode = (int)HttpStatusCode.InternalServerError,
-            };
+                context.Logger.LogLine($"Error deleting product {e.Message} {e.StackTrace}");
+        
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = "Not Found",
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                };
+            }
         }
     }
 }
--- src/NET8/GetProducts/Function.cs
+++ src/NET8/GetProducts/Function.cs
@@ -3,64 +3,48 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using Amazon.XRay.Recorder.Handlers.AwsSdk;
-using Shared;
 using Shared.DataAccess;
 
-namespace GetProducts;
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-public class Function
+namespace GetProducts
 {
-    static ProductsDAO dataAccess;
-
-    static Function()
+    public class Function
     {
-        AWSSDKHandler.RegisterXRayForAllServices();
-        dataAccess = new DynamoDbProducts();
-    }
-
-    /// <summary>
-    /// The main entry point for the custom runtime.
-    /// </summary>
-    /// <param name="args"></param>
-    private static async Task Main()
-    {
-        Func<APIGatewayHttpApiV2ProxyRequest, ILambdaContext, Task<APIGatewayHttpApiV2ProxyResponse>> handler = FunctionHandler;
-        await LambdaBootstrapBuilder.Create(handler, new SourceGeneratorLambdaJsonSerializer<CustomJsonSerializerContext>(options => {
-            options.PropertyNameCaseInsensitive = true;
-        }))
-            .Build()
-            .RunAsync();
-    }
-
-    public static async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest apigProxyEvent, ILambdaContext context)
-    {
-        if (!apigProxyEvent.RequestContext.Http.Method.Equals(HttpMethod.Get.Method))
+        private readonly ProductsDAO dataAccess;
+        public Function()
         {
+            this.dataAccess = new DynamoDbProducts();
+        }
+
+        public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest apigProxyEvent,
+            ILambdaContext context)
+        {
+            if (!apigProxyEvent.RequestContext.Http.Method.Equals(HttpMethod.Get.Method))
+            {
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = "Only GET allowed",
+                    StatusCode = (int)HttpStatusCode.MethodNotAllowed,
+                };
+            }
+    
+            context.Logger.LogLine($"Received {apigProxyEvent}");
+
+            var products = await dataAccess.GetAllProducts();
+    
+            context.Logger.LogLine($"Found {products.Products.Count} product(s)");
+    
             return new APIGatewayHttpApiV2ProxyResponse
             {
-                Body = "Only GET allowed",
-                StatusCode = (int)HttpStatusCode.MethodNotAllowed,
+                Body = JsonSerializer.Serialize(products),
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> {{"Content-Type", "application/json"}}
             };
         }
-        
-        context.Logger.LogInformation($"Received {apigProxyEvent}");
-
-        var products = await dataAccess.GetAllProducts();
-        
-        context.Logger.LogInformation($"Found {products.Products.Count} product(s)");
-        
-        return new APIGatewayHttpApiV2ProxyResponse
-        {
-            Body = JsonSerializer.Serialize(products, CustomJsonSerializerContext.Default.ProductWrapper),
-            StatusCode = 200,
-            Headers = new Dictionary<string, string> {{"Content-Type", "application/json"}}
-        };
     }
 }
