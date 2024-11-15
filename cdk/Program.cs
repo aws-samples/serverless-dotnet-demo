72c2d495c544a74d03fad98367b7541a7ec7b07c
philip pittle <pittle@amazon.com>
2024-11-14 17:19:17 -0800

    added CDK code to create roles used by github actions. Updated TestAuth workflow to use new roles

---
+++ cdk/Program.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.CDK;
+
+namespace AWS.DotnetServerlessDemo.Cdk;
+
+sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var app = new App();
+
+        new GithubOIDCConnectionStack(
+            app,
+            "GithubOIDCConnectionStack",
+            new StackProps
+            {
+                TerminationProtection = true
+            }
+        );
+
+        app.Synth();
+    }
+}
