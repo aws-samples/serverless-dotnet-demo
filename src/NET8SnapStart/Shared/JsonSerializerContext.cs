6b884d22f29fade3957f624c27a801b1b989d394
Beau Gosse <bgosse@amazon.com>
2024-11-21 19:29:30 -0600

    Adding SnapStart.NET8 and update dependencies

---
+++ src/NET8SnapStart/Shared/JsonSerializerContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using Amazon.Lambda.APIGatewayEvents;
+using Shared.Models;
+
+namespace Shared;
+
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
+[JsonSerializable(typeof(List<string>))]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(Product))]
+[JsonSerializable(typeof(ProductWrapper))]
+public partial class CustomJsonSerializerContext : JsonSerializerContext
+{
+}
